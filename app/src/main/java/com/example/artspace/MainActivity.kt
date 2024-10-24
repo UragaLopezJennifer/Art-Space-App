eb98aaec3c4141c61510b4f11f5cdf002215cbd5
Jennifer Uraga Lopez <jenny@jennifers-mbp.mynetworksettings.com>
2024-10-24 12:27:11 -0400

    Added data

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -4,11 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.StringRes
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.example.artspace.data.DataSource
 import com.example.artspace.ui.theme.ArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +29,7 @@ class MainActivity : ComponentActivity() {
             val navController = rememberNavController()
             ArtSpaceTheme {
 
-                NavHost(navController = navController, startDestination = Screen.Home.route) {
+                NavHost(navController = navController, startDestination = Screen.Home.route + "/{id}") {
 
                     composable(
                         Screen.Home.route + "/{id}", arguments = listOf(navArgument("id") {
@@ -27,7 +37,7 @@ class MainActivity : ComponentActivity() {
                             defaultValue = 0
                         })
                     ) {
-                        // TODO: Add HomePage composable
+                        HomePage(navController = navController)
                     }
 
                     composable(
@@ -35,12 +45,29 @@ class MainActivity : ComponentActivity() {
                         arguments = listOf(navArgument("id"){
                             type = NavType.IntType })
                     ) {
-                        // TODO: Add Artist composable
+                        ArtistPage(navController = navController)
                     }
                 }
 
             }
         }
     }
+}
+
+@Composable
+fun ArtistPage (navController: NavController){
+    Text(text = "Artist Page")
+}
+
+@Composable
+fun HomePage(navController: NavController){
+    var current by remember {
+        mutableIntStateOf(
+            navController.currentBackStackEntry?.arguments?.getInt("id") ?: 0
+        )
+    }
+
+    val art = DataSource.arts[current]
+    Text(text="Home Page - ${stringResource(id=art.artistId)}")
 }
 
---
+++ app/src/main/java/com/example/artspace/data/DataSource.kt
@@ -0,0 +1,74 @@
+package com.example.artspace.data
+
+import com.example.artspace.R
+import com.example.artspace.model.Art
+object DataSource {
+    val arts = listOf(
+        Art(
+           R.string.art_1_artist_name,
+           R.string.art_1_artist_info,
+           R.string.art_1_artist_title,
+           R.string.art_1_artist_year,
+           R.string.art_1_artist_description,
+           R.string.art_1_artist_bio,
+           R.drawable.head_pierced_with_arrows,
+           R.drawable.jose_clemente_orozco
+        ),
+
+        Art(
+            R.string.art_2_artist_name,
+            R.string.art_2_artist_info,
+            R.string.art_2_artist_title,
+            R.string.art_2_artist_year,
+            R.string.art_2_artist_description,
+            R.string.art_2_artist_bio,
+            R.drawable.self_portrait,
+            R.drawable.vincent_van_gogh
+        ),
+
+        Art(
+            R.string.art_3_artist_name,
+            R.string.art_3_artist_info,
+            R.string.art_3_artist_title,
+            R.string.art_3_artist_year,
+            R.string.art_3_artist_description,
+            R.string.art_3_artist_bio,
+            R.drawable.medusa,
+            R.drawable.caravaggio
+        ),
+
+        Art(
+            R.string.art_4_artist_name,
+            R.string.art_4_artist_info,
+            R.string.art_4_artist_title,
+            R.string.art_4_artist_year,
+            R.string.art_4_artist_description,
+            R.string.art_4_artist_bio,
+            R.drawable.gewebe,
+            R.drawable.wassily_kandinsky
+        ),
+
+        Art(
+            R.string.art_5_artist_name,
+            R.string.art_5_artist_info,
+            R.string.art_5_artist_title,
+            R.string.art_5_artist_year,
+            R.string.art_5_artist_description,
+            R.string.art_5_artist_bio,
+            R.drawable.dancers_backstage,
+            R.drawable.edgar_degas
+        ),
+
+        Art(
+            R.string.art_6_artist_name,
+            R.string.art_6_artist_info,
+            R.string.art_6_artist_title,
+            R.string.art_6_artist_year,
+            R.string.art_6_artist_description,
+            R.string.art_6_artist_bio,
+            R.drawable.madonna,
+            R.drawable.edvard_munch
+        ),
+
+    )
+}
